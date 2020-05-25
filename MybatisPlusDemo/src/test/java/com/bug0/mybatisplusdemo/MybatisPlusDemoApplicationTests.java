bb852177d5cb195c540b071e46d8aaa7ee74433a
Ruamen <y.frank@msn.com>
2020-05-25 10:49:31 +0800

    first commit from STS

---
+++ MybatisPlusDemo/src/test/java/com/bug0/mybatisplusdemo/MybatisPlusDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bug0.mybatisplusdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisPlusDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
